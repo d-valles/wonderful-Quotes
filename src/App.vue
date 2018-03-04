fc8f061f8086cbe7b70369e7e175e5354eeba4f3
David Valles <david.valles@wizeline.com>
2018-03-04 11:09:59 -0600

    Finished New Quote and delete Quote features

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="container">
-        <appQuoteGrid :quotes="quotes"></appQuoteGrid>
+        <appNewQuote @quoteAdded="newQuote"></appNewQuote>
+        <appQuoteGrid :quotes="quotes" @deletedQuote="deleteQuote"></appQuoteGrid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import QuoteGrid from './components/QuoteGrid.vue'
+    import NewQuote from './components/NewQuote.vue';
+
     export default {
         data() {
             return {
@@ -16,7 +24,16 @@
             }
         },
         components: {
-            appQuoteGrid: QuoteGrid
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+        },
+        methods: {
+            newQuote(quote) {
+                this.quotes.push(quote);
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index,1);
+            }
         }
     }
 </script>
